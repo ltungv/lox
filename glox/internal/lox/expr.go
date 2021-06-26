65e59745402955b1f167907f62d9b497567f3944
Vo Le Tung <15053897+letung3105@users.noreply.github.com>
2021-06-26 21:04:01 +0700

    update makefile, run go fmt

--- glox/internal/lox/expr.go
+++ glox/internal/lox/expr.go
@@ -1,4 +1,5 @@
 package lox
+
 type Expr interface {
 	Accept(visitor ExprVisitor) interface{}
 }
@@ -9,40 +10,47 @@ type ExprVisitor interface {
 	VisitUnaryExpr(expr *UnaryExpr) interface{}
 }
 type BinaryExpr struct {
-	Op *Token
-	Left Expr
+	Op    *Token
+	Left  Expr
 	Right Expr
 }
+
 func NewBinaryExpr(Op *Token, Left Expr, Right Expr) *BinaryExpr {
-	return &BinaryExpr{Op,Left,Right}
+	return &BinaryExpr{Op, Left, Right}
 }
 func (expr *BinaryExpr) Accept(visitor ExprVisitor) interface{} {
 	return visitor.VisitBinaryExpr(expr)
 }
+
 type GroupingExpr struct {
 	Expression Expr
 }
+
 func NewGroupingExpr(Expression Expr) *GroupingExpr {
 	return &GroupingExpr{Expression}
 }
 func (expr *GroupingExpr) Accept(visitor ExprVisitor) interface{} {
 	return visitor.VisitGroupingExpr(expr)
 }
+
 type LiteralExpr struct {
 	Value interface{}
 }
+
 func NewLiteralExpr(Value interface{}) *LiteralExpr {
 	return &LiteralExpr{Value}
 }
 func (expr *LiteralExpr) Accept(visitor ExprVisitor) interface{} {
 	return visitor.VisitLiteralExpr(expr)
 }
+
 type UnaryExpr struct {
-	Op *Token
+	Op         *Token
 	Expression Expr
 }
+
 func NewUnaryExpr(Op *Token, Expression Expr) *UnaryExpr {
-	return &UnaryExpr{Op,Expression}
+	return &UnaryExpr{Op, Expression}
 }
 func (expr *UnaryExpr) Accept(visitor ExprVisitor) interface{} {
 	return visitor.VisitUnaryExpr(expr)
