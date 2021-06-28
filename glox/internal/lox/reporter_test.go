f535f23a66316b7138b169115ec82b49323f7856
Vo Le Tung <15053897+letung3105@users.noreply.github.com>
2021-06-28 23:02:42 +0700

    reporter unittests

---
+++ glox/internal/lox/reporter_test.go
@@ -0,0 +1,76 @@
+package lox
+
+import (
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSimpleReporterInit(t *testing.T) {
+	assert := assert.New(t)
+
+	r := NewSimpleReporter(ioutil.Discard)
+
+	assert.False(r.HadError())
+	assert.False(r.HadRuntimeError())
+}
+
+func TestSimpleReporterSendAnyError(t *testing.T) {
+	assert := assert.New(t)
+	err := errors.New("Test error")
+
+	var out strings.Builder
+	r := NewSimpleReporter(&out)
+	r.Report(err)
+
+	assert.Equal(err.Error(), strings.TrimSpace(out.String()))
+	assert.True(r.HadError())
+	assert.False(r.HadRuntimeError())
+}
+
+func TestSimpleReporterSendRuntimeError(t *testing.T) {
+	assert := assert.New(t)
+	err := NewRuntimeError(NewToken(MINUS, "-", nil, 1), "Operand must be numbers.")
+
+	var out strings.Builder
+	r := NewSimpleReporter(&out)
+	r.Report(err)
+
+	assert.Equal(err.Error(), strings.TrimSpace(out.String()))
+	assert.False(r.HadError())
+	assert.True(r.HadRuntimeError())
+}
+
+func TestSimpleReporterSendErrors(t *testing.T) {
+	assert := assert.New(t)
+	err1 := errors.New("Test error")
+	err2 := NewRuntimeError(NewToken(MINUS, "-", nil, 1), "Operand must be numbers.")
+
+	var out strings.Builder
+	r := NewSimpleReporter(&out)
+	r.Report(err1)
+	r.Report(err2)
+
+	assert.Equal(fmt.Sprintf("%v\n%v", err1, err2), strings.TrimSpace(out.String()))
+	assert.True(r.HadError())
+	assert.True(r.HadRuntimeError())
+}
+
+func TestSimpleReporterReset(t *testing.T) {
+	assert := assert.New(t)
+	err1 := errors.New("Test error")
+	err2 := NewRuntimeError(NewToken(MINUS, "-", nil, 1), "Operand must be numbers.")
+
+	var out strings.Builder
+	r := NewSimpleReporter(&out)
+	r.Report(err1)
+	r.Report(err2)
+
+	r.Reset()
+	assert.False(r.HadRuntimeError())
+	assert.False(r.HadError())
+}
