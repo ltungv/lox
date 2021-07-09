2dc9db0b55f8607a2a065e9f4f2c7052a3e06a5d
Vo Le Tung <15053897+letung3105@users.noreply.github.com>
2021-07-09 23:35:56 +0700

    split files

---
+++ rlox/src/error.rs
@@ -0,0 +1,113 @@
+use std::fmt::{self, Debug};
+
+use crate::Position;
+
+/// Virtual machine errors
+#[derive(Debug)]
+pub enum RuntimeError {
+    /// Pus on an full stack
+    StackOverflow,
+    /// Pop on an empty stack
+    StackUnderflow,
+    /// Wrong arguments given to binary add operators that only accept two numbers
+    /// or two strings
+    InvalidAddOperands(Position),
+    /// Wrong arguments given to binary operators that only accept numbers
+    BinaryNumberOperands(Position),
+    /// Wrong arguments given to unary operators that only accept a numbers
+    UnaryNumberOperand(Position),
+}
+
+/// Error while parsing Lox tokens
+#[derive(Debug)]
+pub enum ParseError {
+    /// Current token is not supposed to be there
+    UnexpectedToken(Position, String, String),
+    /// Reached EOF abruptly
+    UnexpectedEof,
+}
+
+/// Error while scanning Lox source code
+#[derive(Debug, Clone)]
+pub enum ScanError {
+    /// A string literal is unterminated
+    UnterminatedString(Position),
+    /// Invalid character
+    UnexpectedCharacter(Position, char),
+}
+
+/// Lox virtual machine errors
+#[derive(Debug)]
+pub enum Error {
+    /// A runtime error happened
+    Runtime(RuntimeError),
+    /// A compilation error happened
+    Compile,
+}
+
+impl std::error::Error for RuntimeError {}
+impl fmt::Display for RuntimeError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> Result<(), fmt::Error> {
+        match *self {
+            Self::StackOverflow => {
+                writeln!(f, "Virtual machine's stack overflows.")
+            }
+            Self::StackUnderflow => {
+                writeln!(f, "Virtual machine's stack underflows.")
+            }
+            Self::InvalidAddOperands(p) => {
+                writeln!(
+                    f,
+                    "Operands must be two numbers or two strings.\n{} in script.",
+                    p
+                )
+            }
+            Self::BinaryNumberOperands(p) => {
+                writeln!(f, "Operands must be numbers.\n{} in script.", p)
+            }
+            Self::UnaryNumberOperand(p) => {
+                writeln!(f, "Operand must be a number.\n{} in script.", p)
+            }
+        }
+    }
+}
+
+impl std::error::Error for ParseError {}
+impl fmt::Display for ParseError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::UnexpectedToken(ref pos, ref lexeme, ref msg) => {
+                write!(f, "{} Error at '{}': {}.", pos, lexeme, msg,)
+            }
+            Self::UnexpectedEof => write!(f, "Error: Unexpected end of file."),
+        }
+    }
+}
+
+impl std::error::Error for ScanError {}
+impl fmt::Display for ScanError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::UnterminatedString(pos) => write!(f, "{} Error: Unterminated string.", pos),
+            Self::UnexpectedCharacter(pos, c) => {
+                write!(f, "{} Error: Unexpected character '{}'.", pos, c)
+            }
+        }
+    }
+}
+
+impl std::error::Error for Error {}
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::result::Result<(), std::fmt::Error> {
+        match self {
+            Self::Runtime(err) => write!(f, "{}", err),
+            Self::Compile => write!(f, "Compilation errors."),
+        }
+    }
+}
+
+impl From<RuntimeError> for Error {
+    fn from(err: RuntimeError) -> Self {
+        Self::Runtime(err)
+    }
+}
---
+++ rlox/src/opcode.rs
@@ -0,0 +1,51 @@
+/// OpCode is a number that specifies the type of the instruction.
+///
+/// # Notes
+///
+/// If it was for performances purposes, the following options could be considered:
+/// + Having the opcode designed to be as close as possbible to existing lower-level instructions
+/// + Having specialized opcode for constant
+///
+/// We don't have a `OpCode::NotEqual` because we will transform `a != b` to `!(a == b)` to demonstrated
+/// that bytecode can deviate from the actual user's code as long as they behave similarly. This is
+/// also applied for operator `<=` and operator `>=`.
+///
+/// `a <= b` does not equals equivalent to `!(a > b)`, similarly with greater and greater or equal.
+/// According to [IEEE 754] all comparison operators return `false` when an operand is `NaN`. These
+/// are implementation details that we should keep in mind when making a real language.
+///
+/// [IEEE 754]: https://en.wikipedia.org/wiki/IEEE_754
+#[derive(Debug, Clone, Copy)]
+#[repr(u8)]
+pub enum OpCode {
+    /// Load a constant
+    Constant(u8),
+    /// Load a `nil` value
+    Nil,
+    /// Load a `true` value
+    True,
+    /// Load a `false` value
+    False,
+    /// Return from the current function
+    Return,
+    /// Apply logical `not` to a single boolean operand
+    Not,
+    /// Negate a single number operand
+    Negate,
+    /// Check for equality between 2 operands.
+    Equal,
+    /// Compare if the first operand is greater than the second
+    Greater,
+    /// Compare if the first operand is less than the second
+    Less,
+    /// Add two number operands
+    Add,
+    /// Subtract the first operand with the second operand, both operands
+    /// must be numbers
+    Subtract,
+    /// Multiply two number operands
+    Multiply,
+    /// Divide the first operand with the second operand, both operands
+    /// must be numbers
+    Divide,
+}
